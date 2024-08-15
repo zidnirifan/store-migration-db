717423d70e42b50ce2689a8e2ce1e09c6430e339
zidnirifan <zidni.rifan7@gmail.com>
2024-08-15 10:19:38 +0700

    create table categories and products

---
+++ migrations/20240815030157_create_table_products.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  price BIGINT NOT NULL,
+  category_id INTEGER NOT NULL,
+  stock INTEGER NOT NULL,
+  images TEXT[] NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+);
